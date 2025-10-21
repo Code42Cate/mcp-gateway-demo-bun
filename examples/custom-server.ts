e6014dbfae009f59f8132be8aa3d90c2176b6163
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2025-10-20 17:16:22 -0700

    add custom server demo

---
+++ examples/custom-server.ts
@@ -0,0 +1,40 @@
+import Sandbox from 'e2b'
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js';
+
+const sandbox = await Sandbox.betaCreate({
+    mcp: {
+        'github/modelcontextprotocol/servers': {
+            installCmd: 'npm install',
+            runCmd: 'npx -y @modelcontextprotocol/server-filesystem /root',
+        },
+    },
+    timeoutMs: 600_000,
+});
+
+
+const client = new Client({
+    name: 'streamable-e2b-gateway-client',
+    version: '1.0.0'
+});
+
+
+const transport = new StreamableHTTPClientTransport(new URL(sandbox.betaGetMcpUrl()), {
+     requestInit: {
+        headers: {
+            'Authorization': `Bearer ${await sandbox.betaGetMcpToken()}`
+        }
+     }
+});
+
+await client.connect(transport);
+
+console.log('Connected to MCP server');
+
+const tools = await client.listTools();
+for (const tool of tools.tools) {
+    console.log(tool.name);
+}
+
+console.log('Disconnecting from MCP server');
+await client.close();
